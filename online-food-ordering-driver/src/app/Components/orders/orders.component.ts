a6d9be56f9204f7b0d299f49a231929c7744d7b4
ignatius <ignatius.mafahla@dadevs.co.za>
2023-03-07 08:00:12 +0200

    orders

--- online-food-ordering-driver/src/app/Components/orders/orders.component.ts
+++ online-food-ordering-driver/src/app/Components/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersServiceService } from 'src/app/Services/orders-service.service';
 
 @Component({
   selector: 'app-orders',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+
+
+  orders: any[] = [];
+  order:any
+  cartDetails: any;
+
+  constructor(private orderService: OrdersServiceService) { }
+
+
+
+  public selector( id:any){
+    this.orderService.getOrderById(id).subscribe((booking: any) =>{
+      this.order = booking.data;
+      this.cartDetails = this.order.attributes.cartDetails;
+      console.log(this.cartDetails, "qwerty")
+    })
+  }
 
   ngOnInit(): void {
+    this.orderService.getOrders().subscribe((booking: any) =>{
+      this.orders = booking.data;
+      console.log(this.orders)
+    })
+   
   }
+  
 
 }
